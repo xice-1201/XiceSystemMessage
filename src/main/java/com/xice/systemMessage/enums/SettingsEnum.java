f327aadb92309494f8832a1a503b3100d35c6ea3
Xice玄冰 <1500065926@qq.com>
2026-02-20 17:26:10 +0800

    FIX: 修复配置文件读写错误的问题
    - 将配置文件中保存的所有 Enum 更换为 String

--- src/main/java/com/xice/systemMessage/enums/SettingsEnum.java
+++ src/main/java/com/xice/systemMessage/enums/SettingsEnum.java
@@ -4,11 +4,11 @@ import javax.annotation.Nonnull;
 
 public enum SettingsEnum {
   LOGIN_MESSAGE_ENABLE("login-message.enable", false, "是否启用自定义玩家登录消息"),
-  LOGIN_MESSAGE_VISIBILITY("login-message.visibility", MessageVisibilityEnum.GLOBAL, "自定义登录消息可见性（disable：不可见；self：仅自己；nearby：附近玩家；world：当前世界；global：全服务器）"),
+  LOGIN_MESSAGE_VISIBILITY("login-message.visibility", MessageVisibilityEnum.GLOBAL.getValue(), "自定义登录消息可见性（disable：不可见；self：仅自己；nearby：附近玩家；world：当前世界；global：全服务器）"),
   LOGIN_MESSAGE_CONTENT("login-message.content", "<green>欢迎 %player% 加入服务器！", "自定义登录消息内容（支持 MiniMessage 和占位符）"),
   LOGIN_MESSAGE_RANGE("login-message.range", 10.0, "若 " + SettingsEnum.LOGIN_MESSAGE_VISIBILITY.getKey() + " 为 nearby，该值决定登录者多少格范围内的玩家可以看到登录消息"),
   QUIT_MESSAGE_ENABLE("quit-message.enable", false, "是否启用自定义玩家退出消息"),
-  QUIT_MESSAGE_VISIBILITY("quit-message.visibility", MessageVisibilityEnum.GLOBAL, "自定义登录消息可见性（disable：不可见；self：仅自己；nearby：附近玩家；world：当前世界；global：全服务器）"),
+  QUIT_MESSAGE_VISIBILITY("quit-message.visibility", MessageVisibilityEnum.GLOBAL.getValue(), "自定义登录消息可见性（disable：不可见；self：仅自己；nearby：附近玩家；world：当前世界；global：全服务器）"),
   QUIT_MESSAGE_CONTENT("quit-message.content", "<red>%player% 离开了服务器！", "自定义登录消息内容（支持 MiniMessage 和占位符）"),
   QUIT_MESSAGE_RANGE("quit-message.range", 10.0, "若 " + SettingsEnum.QUIT_MESSAGE_VISIBILITY.getKey() + " 为 nearby，该值决定退出者多少格范围内的玩家可以看到退出消息");
 
